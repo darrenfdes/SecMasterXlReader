f67be7e16e2b8209d370763911787cf64b74a3bb
Darren Fernandes <46609190+darrenfdes@users.noreply.github.com>
2021-09-15 12:09:28 +0530

    Update Bond.cs

--- uploadCSV/Bond.cs
+++ uploadCSV/Bond.cs
@@ -1,146 +1,142 @@
 using System;
+using System.Collections.Generic;
 
 
 namespace uploadCSV
 {
-    public class Bond{
-        public string AttributeName {get; set;}
-        public string SecurityDescription {get; set;}
+    class Equity
+    {
+        public int SID {get; set;}
         public string SecurityName {get; set;}
-        public string AssetType {get; set;}
-        public string InvestmentType {get; set;}
-        public double TradingFactor {get; set;}
-        public double PricingFactor {get; set;}
-        public string ISIN {get; set;}
-        public string BBGTicker {get; set;}
-        public string BBGUniqueID {get; set;}
-        public string CUSIP {get; set;}
-        public string SEDOL {get; set;}
-        public string FirstCouponDate {get; set;}       // Set to string as datetype not available.
-        public string Cap {get; set;}   
-        public string Floor {get; set;}
-        public int CouponFrequency {get; set;}
-        public double Coupon {get; set;}
-        public string CouponType {get; set;}
-        public string Spread {get; set;}
-        public bool CallableFlag {get; set;}
-        public bool FixtoFloatFlag {get; set;}
-        public bool PutableFlag {get; set;}
-        public string IssueDate {get; set;}
-        public string LastResetDate {get; set;}
-        public string Maturity {get; set;} 
-        public double CallNotificationMaxDays {get; set;}
-        public string PutNotificationMaxDays {get; set;}
-        public string PenultimateCouponDate {get; set;} 
-        public string ResetFrequency {get; set;} 
+        public string SecurityDescription {get; set;}
         public bool HasPosition {get; set;}
-        public double MacaulayDuration {get; set;}
-        public string Volatility30D {get; set;}  
-        public string Volatility90D {get; set;}
-        public double Convexity {get; set;}
-        public string AverageVolume30Day {get; set;}
+        public bool IsActiveSecurity {get; set;}
+        public int LotSize {get; set;}
+        public string BBGUniqueName {get; set;}
+        public string CUSIP {get; set;}
+        public string ISIN {get; set;}
+        public string SEDOL {get; set;}
+        public string BloombergTicker {get; set;}
+        public string BloombergUniqueID {get; set;}
+        public string BBGGlobalID {get; set;}
+        public string TickerandExchange {get; set;}
+        public bool IsADRFlag {get; set;}
+        public string ADRUnderlyingTicker {get; set;}
+        public string ADRUnderlyingCurrency {get; set;}
+        public string SharesPerADR {get; set;}
+        public string IPODate {get; set;}
+        public string PricingCurrency {get; set;}
+        public int SettleDays {get; set;}
+        public double TotalOutStandingShares {get; set;}
+        public double VotingRightsPerShare {get; set;}
+        public double AverageVolume20Day {get; set;}
+        public double Beta {get; set;}
+        public double ShortInterest {get; set;}
+        public double ReturnYTD {get; set;}
+        public double Volatility90D {get; set;}
         public string PFAssetClass {get; set;}
         public string PFCountry {get; set;}
         public string PFCreditRating {get; set;}
         public string PFCurrency {get; set;}
         public string PFInstrument {get; set;}
         public string PFLiquidityProfile {get; set;}
-        public string PFMaturity {get; set;} 
+        public string PFMaturity {get; set;}
         public string PFNAICSCode {get; set;}
         public string PFRegion {get; set;}
         public string PFSector {get; set;}
         public string PFSubAssetClass {get; set;}
-        public string BloombergIndustryGroup {get; set;}
-        public string BloombergIndustrySubGroup {get; set;}
-        public string BloombergIndustrySector {get; set;}
-        public string CountryofIssuance{get; set;}
-        public string IssueCurrency {get; set;}
+        public string CountryofIssuance {get; set;}
+        public string Exchange {get; set;}
         public string Issuer{get; set;}
+        public string IssueCurrency{get; set;}
+        public string TradingCurrency {get; set;}
+        public string BBGIndustrySubGroup {get; set;}
+        public string BloombergIndustryGroup {get; set;}
+        public string BloombergSector {get; set;}
+        public string CountryofIncorporation {get; set;}
         public string RiskCurrency {get; set;}
-        public string PutDate {get; set;} 
-        public double PutPrice {get; set;} 
-        public double AskPrice {get; set;}
-        public double HighPrice {get; set;}
-        public double LowPrice {get; set;}
         public double OpenPrice {get; set;}
+        public double ClosePrice {get; set;}
         public double Volume {get; set;}
-        public double BidPrice {get; set;}
         public double LastPrice {get; set;}
-        public string CallDate {get; set;}
-        public double CallPrice {get; set;}
-        
-        public Bond(string row){
+        public double AskPrice {get; set;}
+        public double BidPrice {get; set;}
+        public double PERatio {get; set;}
+        public string DividendDeclaredDate {get; set;} 
+        public string DividendExDate {get; set;} 
+        public string DividendRecordDate {get; set;} 
+        public string DividendPayDate {get; set;} 
+        public double DividendAmount {get; set;}
+        public string Frequency {get; set;} 
+        public string DividendType {get; set;}
+
+        public Equity(string row){
+
             string[]data = row.Split('|','\n');
             
-            
-                
-
-            
             try{
-                AttributeName = data[0];
-                SecurityDescription = data[1];
-                SecurityName = data[2];
-                AssetType = data[3];
-                InvestmentType = data[4];
-                TradingFactor = Convert.ToDouble(data[5].Equals("") ? null : data[5]);
-                PricingFactor = Convert.ToDouble(data[6].Equals("") ? null : data[6]);
-                ISIN = data[7];
-                BBGTicker = data[8];
-                BBGUniqueID = data[9];
-                CUSIP = data[10];
-                SEDOL = data[11];
-                FirstCouponDate = data[12];
-                Cap = data[13];
-                Floor = data[14];
-                CouponFrequency = Convert.ToInt32(data[15].Equals("") ? null : data[15]);
-                Coupon = Convert.ToDouble(data[16].Equals("") ? null : data[16]);
-                CouponType = data[17];
-                Spread = data[18];
-                CallableFlag = Convert.ToBoolean(data[19]);
-                FixtoFloatFlag = Convert.ToBoolean(data[20]);
-                PutableFlag = Convert.ToBoolean(data[21]);
-                IssueDate = data[22];
-                LastResetDate = data[23];
-                Maturity = data[24];
-                CallNotificationMaxDays = Convert.ToDouble(data[25].Equals("") ? null : data[25]);
-                PutNotificationMaxDays = data[26];
-                PenultimateCouponDate = data[27];
-                ResetFrequency = data[28];
-                HasPosition = Convert.ToBoolean(data[29]);
-                MacaulayDuration = Convert.ToDouble(data[30].Equals("") ? null : data[30]);
-                Volatility30D = data[31];
-                Volatility90D = data[32];
-                Convexity = Convert.ToDouble(data[34].Equals("") ? null : data[33]);
-                AverageVolume30Day = data[34];
-                PFAssetClass = data[35];
-                PFCountry = data[36];
-                PFCreditRating = data[37];
-                PFCurrency = data[38];
-                PFInstrument = data[39];
-                PFLiquidityProfile = data[40];
-                PFMaturity = data[41];
-                PFNAICSCode = data[42];
-                PFRegion = data[43];
-                PFSector = data[44];
-                PFSubAssetClass = data[45];
-                BloombergIndustryGroup = data[46];
-                BloombergIndustrySubGroup = data[47];
-                BloombergIndustrySector = data[48];
-                CountryofIssuance = data[49];
-                IssueCurrency = data[50];
-                Issuer = data[51];
-                RiskCurrency = data[52];
-                PutDate = data[53];
-                PutPrice = Convert.ToDouble(data[54].Equals("") ? null : data[54]);
-                AskPrice = Convert.ToDouble(data[55].Equals("") ? null : data[55]);
-                HighPrice = Convert.ToDouble(data[56].Equals("") ? null : data[56]);
-                LowPrice = Convert.ToDouble(data[57].Equals("") ? null : data[57]);
-                OpenPrice = Convert.ToDouble(data[58].Equals("") ? null : data[58]);
-                Volume = Convert.ToDouble(data[59].Equals("") ? null : data[59]);
-                BidPrice = Convert.ToDouble(data[60].Equals("") ? null : data[60]);
-                LastPrice = Convert.ToDouble(data[61].Equals("") ? null : data[61]);
-                CallDate = data[62];
-                CallPrice = Convert.ToDouble(data[63].Equals("") ? null : data[63]);
+                SecurityName = data[1];
+                SecurityDescription =  data[2];
+                HasPosition = Convert.ToBoolean(data[3].Equals("") ? null : data[3]);
+                IsActiveSecurity = Convert.ToBoolean(data[4].Equals("") ? null : data[4]);
+                LotSize = Convert.ToInt32(data[5].Equals("") ? null : data[5]);
+                BBGUniqueName = data[6];
+                CUSIP = data[7];
+                ISIN = data[8];
+                SEDOL =  data[9];
+                BloombergTicker = data[10];
+                BloombergUniqueID = data[11];
+                BBGGlobalID =   data[12];
+                TickerandExchange =      data[13];
+                IsADRFlag = Convert.ToBoolean(data[14].Equals("") ? null : data[14]);
+                ADRUnderlyingTicker =  data[15];
+                ADRUnderlyingCurrency = data[16];
+                SharesPerADR = data[17];
+                IPODate = data[18].Equals("")? null : DateTime.Parse(data[18]).ToString("MM-dd-yyyy");
+                PricingCurrency = data[19];
+                SettleDays = Convert.ToInt32(data[20].Equals("") ? null : data[20]);
+                TotalOutStandingShares = Convert.ToDouble(data[21].Equals("") ? null : data[21]);
+                VotingRightsPerShare = Convert.ToDouble(data[22].Equals("") ? null : data[22]);
+                AverageVolume20Day = Convert.ToDouble(data[23].Equals("") ? null : data[23]);
+                Beta = Convert.ToDouble(data[24].Equals("") ? null : data[24]);
+                ShortInterest = Convert.ToDouble(data[25].Equals("") ? null : data[25]);
+                ReturnYTD = Convert.ToDouble(data[26].Equals("") ? null : data[26]);
+                Volatility90D = Convert.ToDouble(data[27].Equals("") ? null : data[27]);
+                PFAssetClass =  data[28];
+                PFCountry = data[29];
+                PFCreditRating =  data[30];
+                PFCurrency = data[31];
+                PFInstrument =  data[32];
+                PFLiquidityProfile =  data[33];
+                PFMaturity =  data[34];
+                PFNAICSCode =  data[35];
+                PFRegion =  data[36];
+                PFSector =  data[37];
+                PFSubAssetClass = data[38];
+                CountryofIssuance =  data[39];
+                Exchange = data[40];
+                Issuer =  data[41];
+                IssueCurrency = data[42];
+                TradingCurrency = data[43];
+                BBGIndustrySubGroup =  data[44];
+                BloombergIndustryGroup =  data[45];
+                BloombergSector =  data[46];
+                CountryofIncorporation =  data[47];
+                RiskCurrency =  data[48];
+                OpenPrice = Convert.ToDouble(data[49].Equals("") ? null : data[49]);
+                ClosePrice = Convert.ToDouble(data[50].Equals("") ? null : data[50]);
+                Volume = Convert.ToDouble(data[51].Equals("") ? null : data[51]);
+                LastPrice = Convert.ToDouble(data[52].Equals("") ? null : data[52]);
+                AskPrice = Convert.ToDouble(data[53].Equals("") ? null : data[53]);
+                BidPrice = Convert.ToDouble(data[54].Equals("") ? null : data[54]);
+                PERatio = Convert.ToDouble(data[55].Equals("") ? null : data[55]);
+                DividendDeclaredDate = data[18].Equals("")? null : DateTime.Parse(data[18]).ToString("MM-dd-yyyy");
+                DividendExDate = data[18].Equals("")? null : DateTime.Parse(data[18]).ToString("MM-dd-yyyy");
+                DividendRecordDate = data[18].Equals("")? null : DateTime.Parse(data[18]).ToString("MM-dd-yyyy");
+                DividendPayDate =data[18].Equals("")? null : DateTime.Parse(data[18]).ToString("MM-dd-yyyy");
+                DividendAmount = Convert.ToDouble(data[60].Equals("") ? null : data[60]);
+                Frequency =data[61];
+                DividendType = data[62];
             }
             catch(FormatException e){
                 System.Console.WriteLine("{0}{1}",e,SecurityName);
@@ -151,7 +147,7 @@ namespace uploadCSV
             finally
             {
                 System.Console.WriteLine("Security Name "+SecurityName+" Inserted");
-                System.Console.WriteLine("BloombergUniqueId "+BBGUniqueID);
+                System.Console.WriteLine("BloombergUniqueId "+BloombergUniqueID);
             }
         }
     }
